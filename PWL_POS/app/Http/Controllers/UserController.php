2c4190987c45049e1333036117a87182cd8cb524
nurill <mnurilhuda2005@gmail.com>
2026-02-28 13:57:41 +0700

    jobsheet 3 praktikum 6

---
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        
+        //tambah data user dengan ELoquen1 Model
+        // $data = [
+        //     'username' => 'customer-1',
+        //     'nama' => 'Pelanggan',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2
+        // ];
+        // UserModel::insert($data); //tambahkan data baru ke tabel m_user
+
+        //update data user dengan Eloquent Model
+        $data = [
+            'nama' => 'Pelanggan Pertama',
+            'level_id' => 5
+        ];
+        UserModel::where('username', 'customer-1')->update($data); //update data user
+
+        //coba akses model UserModekl untuk mengambil semua data user
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+    }
+}
---
+++ PWL_POS/app/Models/UserModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class UserModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'm_user'; // Mendifinisakan nama tabel yang digunakan oleh model ini
+    protected $primaryKey = 'user_id'; // Mendifinisikan primary key dari tabel yang digunakan
+
+}
