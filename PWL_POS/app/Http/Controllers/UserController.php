caebcdc9b5e6dcf253aa824b6fcbf705d21d3992
nurill <mnurilhuda2005@gmail.com>
2026-03-05 14:24:16 +0700

    Jobsheet 4 praktikum 2.4

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -10,7 +10,15 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::where('username','manager9')->firstOrFail();
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ]
+        );
+        $user->save();
         return view('user', ['data' => $user]);
     }
 }
