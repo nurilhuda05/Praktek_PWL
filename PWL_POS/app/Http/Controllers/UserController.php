dcebef55695007a09ef6264a800734093c2a884a
nurill <mnurilhuda2005@gmail.com>
2026-03-05 14:40:32 +0700

    jobsheet 4 praktikum 2.5

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -10,37 +10,21 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstOrNew(
+        $user = UserModel::create(
             [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
+                'username' => 'manager11',
+                'nama' => 'Manager11',
                 'password' => Hash::make('12345'),
                 'level_id' => 2
             ]
         );
+        $user->username = 'manager12';
         $user->save();
-        return view('user', ['data' => $user]);
+
+        $user->wasChanged(); // true
+        $user->wasChanged('username'); // true
+        $user->wasChanged(['username','level_id']); // true
+        $user->wasChanged('nama'); // false
+        dd($user->wasChanged(['nama','username'])); // true
     }
 }
-//tambah data user dengan ELoquen1 Model
-        // $data = [
-        //     'username' => 'customer-1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 2
-        // ];
-        // UserModel::insert($data); //tambahkan data baru ke tabel m_user
-
-        //update data user dengan Eloquent Model
-        // $data = [
-        //     'nama' => 'Pelanggan Pertama',
-        //     'level_id' => 5
-        // ];
-        // UserModel::where('username', 'customer-1')->update($data); //update data user
-        // $data =[
-        //     'level_id' => 2,
-        //     'username' => 'manager_tiga',
-        //     'nama' => 'Manager Tiga',
-        //     'password' => Hash::make('12345')
-        // ];
-        // UserModel::create($data); 
