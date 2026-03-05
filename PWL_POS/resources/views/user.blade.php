befe4f40247238faba7d08b6d345c7511429717f
nurill <mnurilhuda2005@gmail.com>
2026-03-05 15:34:30 +0700

    Jobsheet 4 praktikum 2.6

--- PWL_POS/resources/views/user.blade.php
+++ PWL_POS/resources/views/user.blade.php
@@ -1,22 +1,27 @@
-<html>
-    <head>
-        <title>Data User</title>
-    </head>
-    <body>
-        <h1>Data User</h1>
-        <table border="1" cellpadding="2" cellspacing="0">
-            <tr>
-                <th>ID</th>
-                <th>Username</th>
-                <th>Nama</th>
-                <th>Level ID Pengguna</th>
-            </tr>
-            <tr>
-                <td>{{ $data->user_id }}</td>
-                <td>{{ $data->username }}</td>
-                <td>{{ $data->nama }}</td>
-                <td>{{ $data->level_id }}</td>
-            </tr>
-        </table>
-    </body>
-</html
+<body>
+    <h1>Data User</h1>
+    <a href="/PWL/PWL_POS/public/user/tambah">+ Tambah User</a>
+    
+    <table border="1" cellpadding="2" cellspacing="0">
+        <tr>
+            <td>ID</td>
+            <td>Username</td>
+            <td>Nama</td>
+            <td>ID Level Pengguna</td>
+            <td>Aksi</td>
+        </tr>
+        
+        @foreach ($data as $d)
+        <tr>
+            <td>{{ $d->user_id }}</td>
+            <td>{{ $d->username }}</td>
+            <td>{{ $d->nama }}</td>
+            <td>{{ $d->level_id }}</td>
+            <td>
+                <a href="/PWL/PWL_POS/public/user/ubah/{{ $d->user_id }}">Ubah</a> | 
+                <a href="/PWL/PWL_POS/public/user/hapus/{{ $d->user_id }}">Hapus</a>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+</body>
